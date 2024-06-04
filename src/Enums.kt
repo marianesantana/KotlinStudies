efd7afd7a8ed60363a441b3e234482f5d2d7d320
marianesantana <mariane.santana@bairesdev.com>
2024-06-04 12:30:33 -0300

    Enums and Sealed classes

--- src/Enums.kt
+++ src/Enums.kt
@@ -1,20 +1,87 @@
 fun main () {
-    val input = Result.SUCCESS
-    getResult(input)
+    Repository.startFetch()
+    getResult(result = Repository.getCurrentState())
+    Repository.finishFetch()
+    getResult(result = Repository.getCurrentState())
+    Repository.errorFetch()
+    getResult(result = Repository.getCurrentState())
+    Repository.customFailure()
+    getResult(result = Repository.getCurrentState())
+    Repository.anotherCustomFailure()
+    getResult(result = Repository.getCurrentState())
+
 
 }
 
 fun getResult(result: Result) {
     return when(result){
-        Result.SUCCESS -> println("Success! UHU")
-        Result.ERROR -> println("Error! :(")
-        Result.FAILURE -> println("Failure....")
+        is Error -> println("Error! ${result.error}")
+        is Success -> println("Success! UHU ${result.dataFetched?: "Ensure you start Fetching."}")
+        is Loading -> println("Loading...")
+        is NotLoading -> println("IDLE")
+        is Failure.AnotherCustomFailure -> println("Another CustomFailure: ${result.dataFetched.toString()}")
+        is Failure.CustomFailure -> println("CustomFailure: ${result.dataFetched.toString()}")
+
+
+//        Result.SUCCESS -> println("Success! UHU")
+//        Result.ERROR -> println("Error! :(")
+//        Result.IDLE -> println("Nothing happens")
+//        Result.LOADING -> println("Loading.....")
+
     }
 }
 
- enum class Result {
-    SUCCESS,
-    ERROR,
-    FAILURE
+sealed class Result
+data class Success(val dataFetched: String?): Result()
+data class Error(val error: Exception): Result()
+object Loading: Result()
+object NotLoading: Result()
+
+sealed class Failure: Result() {
+    data class CustomFailure(val dataFetched: String?): Failure()
+    data class AnotherCustomFailure(val dataFetched: String?): Failure()
+}
+
+
+// enum class Result {
+//     SUCCESS,
+//     ERROR,
+//     IDLE,
+//     LOADING
+//}
+
+object Repository {
+    private var loadState : Result = NotLoading
+    private var dataFetched: String? = null
+
+    fun startFetch(){
+        loadState = Loading
+        dataFetched = "data"
+    }
+
+    fun finishFetch() {
+        loadState = Success(dataFetched)
+        dataFetched = null
+    }
+
+    fun errorFetch() {
+        loadState = Error(error = Exception("Exception"))
+
+    }
+
+    fun customFailure(){
+        loadState = Failure.CustomFailure(dataFetched = "NullPointer Exception")
+    }
+
+    fun anotherCustomFailure(){
+        loadState = Failure.AnotherCustomFailure(dataFetched = "IO Exception")
+    }
+
+
+    fun getCurrentState(): Result {
+        return loadState
+    }
+
+
 }
 
